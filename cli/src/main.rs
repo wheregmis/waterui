981055cb74fdb40ec9ff49da732f45d170ffebd5
Lexo Liu <me@lexo.cool>
2025-09-12 10:32:02 +0800

    feat: add CLI package with initial command structure and dependencies

---
+++ cli/src/main.rs
@@ -0,0 +1,41 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(name = "water")]
+#[command(about = "CLI of WaterUI", long_about = None)]
+#[command(version, author)]
+struct Cli {
+    #[arg(short, long, global = true)]
+    verbose: bool,
+
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    New {
+        // Setting up all backends by default
+    },
+    Run {
+        // Hot reload enabled by default
+    },
+    Build {},
+    I18n {},
+
+    Plugin {},
+
+    Backend {},
+}
+
+fn main() {
+    let cli = Cli::parse();
+}
+
+pub fn add_swiftui_backend() {
+    println!("Adding SwiftUI backend...");
+}
+
+pub fn add_android_backend() {
+    println!("Adding Android backend...");
+}
