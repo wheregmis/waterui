d3e5eeec3e4579a354c0488d0db06b1146ab31b0
Lexo Liu <me@lexo.cool>
2025-11-01 20:11:23 +0800

    fix: Fix git submodule

--- backends/apple/Sources/CWaterUI/WaterUI.c
+++
@@ -1,3 +0,0 @@
-// This file exists to satisfy Swift Package Manager's requirement
-// for a C target to have at least one source file.
-// The actual implementation is in the Rust library.
--- backends/apple/Sources/WaterUI/Alert.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Alert.swift
-//  waterui-swift
-//
-//  Created by Lexo Liu on 11/6/24.
-//
-
-
